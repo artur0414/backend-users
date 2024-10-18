fb26bd1a7a154adc3319da40930fc6139dd1b992
Artur Dev <artur.acost0414@gmail.com>
2024-10-18 11:59:34 -0500

    updating allowed cors

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -4,7 +4,7 @@ import cors from "cors";
 
 const ACCEPTED_ORIGINS = [
   "http://localhost:3000",
-  "https://frontend-users-4iey6mj43-arturo-acostas-projects.vercel.app",
+  "https://frontend-users-bwsc16525-arturo-acostas-projects.vercel.app",
 ];
 
 export const corsMiddleware = ({ acceptedCors = ACCEPTED_ORIGINS } = {}) => {
