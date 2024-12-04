e56a255f456a4013ff3c03294d69ab12c391e542
Pedro Arturo Acosta Sánchez <167545086+artur0414@users.noreply.github.com>
2024-12-04 08:40:29 -0500

    Update cors.js

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -17,5 +17,7 @@ export const corsMiddleware = ({ acceptedCors = ACCEPTED_ORIGINS } = {}) => {
       }
     },
     credentials: true,
+        methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    allowedHeaders: "Content-Type,Authorization",
   });
 };
